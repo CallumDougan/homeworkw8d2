4f22eb6fc59f95bf27b09a15f4a259c89f85b1de
CallumDougan <practicalitymalfunction@gmail.com>
2016-01-26 15:30:53 +0000

    basket now calculated base price without modifiers

--- shopping_basket.js
+++ shopping_basket.js
@@ -3,16 +3,30 @@ var myBasket = {
   price: 0,
   discount_card: true,
   price_checker: function(items){
-
+    priceTally = 0
+    for(item of items){
+      priceTally += (item.price * item.number_of)
+    }
+    this.price = priceTally
   } 
+}
+
+var testItem = {
+  price: 3,
+  number_of: 3
 }
 
 var firstItem = {
   price: 5,
-  deal: true
+  deal: true,
+  number_of: 0
 }
 
 var secondItem = {
   price: 2,
-  deal: false
+  deal: false,
+  number_of: 0
 }
+
+
+module.exports = [myBasket, testItem]
--- shopping_basket_spec.js
+++ shopping_basket_spec.js
@@ -1,2 +1,11 @@
-var basket = require('./shopping_basket');
+var basket = require('./shopping_basket')[0];
+var testItem = require('./shopping_basket')[1]
 var assert = require('assert');
+
+describe('basket', function(){
+  it('should add prices together', function(){
+    basket.items.push(testItem)
+    basket.price_checker(basket.items);
+    assert.equal(9, basket.price)
+  })
+})
