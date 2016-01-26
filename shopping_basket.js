2ae463ac8f4a24c9e71b44e0bebb38d45ec83f65
CallumDougan <practicalitymalfunction@gmail.com>
2016-01-26 14:54:24 +0000

    added secondItem

--- shopping_basket.js
+++ shopping_basket.js
@@ -1,10 +1,18 @@
 var myBasket = {
   items: [],
-  price: 0
-  discount_card: true
+  price: 0,
+  discount_card: true,
+  price_checker: function(items){
+
+  } 
 }
 
 var firstItem = {
-  price: 5
+  price: 5,
   deal: true
+}
+
+var secondItem = {
+  price: 2,
+  deal: false
 }
